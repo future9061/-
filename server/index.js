6f5e796f94bed5fb4166ffa0eb2b6bcb52792020
leemirae <416homin@daum.net>
2023-09-06 22:59:34 +0900

    first

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const path = require("path")
+const app = express()
+const port = 5000
+const mongoose = require('mongoose');
+
+app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(port, () => {
+  mongoose.connect('mongodb+srv://leemirae:10zapJSlHVxolpyK@cluster0.gi7m1r1.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+      console.log(`localhost ${port} 실행 중`)
+      console.log(`connecting MongoDB...`)
+    })
+    .catch((err) => {
+      console.log(`${err} ㅠㅠ`)
+    })
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
+app.post('/api/test', (req, res) => {
+  console.log(req.body)
+  res.status(200).json({ success: true, text: 'list' })
+})
+
